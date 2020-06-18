d74d91a03022761a06608ff801a8e21a08af213e
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-06-18 13:56:47 -0700

    test mainpage

---
+++ src/components/MainPage.test.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import MainPage from './MainPage'
+
+let wrapper
+
+beforeEach(() => {
+    const mockProps = {
+        onRequestRobots: jest.fn(),
+        robots: [],
+        searchField: '',
+        isPending: false,
+    }
+    wrapper = shallow(<MainPage {...mockProps} />)
+})
+
+
+
+it('expect to render MainPage component without crashing', () => {
+
+    expect(wrapper.debug()).toMatchSnapshot()
+});
+
+it('filters robots correctly', () => {
+    const mockProps2 = {
+        onRequestRobots: jest.fn(),
+        robots: [{
+            id: 3,
+            name: 'John',
+            email: 'john@gmail.com'
+        }],
+        searchField: 'john',
+        isPending: false,
+    }
+    const wrapper2 = shallow(<MainPage {...mockProps2} />)
+
+    expect(wrapper2.instance().filteredRobots()).toEqual([{
+        id: 3,
+        name: 'John',
+        email: 'john@gmail.com'
+    }])
+})
